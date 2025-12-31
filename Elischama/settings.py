f21b82e77a65be5b732a7266cb9b41c3c47e5690
Fofana Mamadou <muhammadfofana367@gmail.com>
2025-12-31 07:41:19 +0000

    otra comit para desplegar

--- Elischama/settings.py
+++ Elischama/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l@z=7azp)en(%0ik_%9cmeo6=4*qb#00&yl3y+85gr2jddc(h@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["edichama.onrender.com"]  # reemplaza con tu dominio Render
 
 
 # Application definition
